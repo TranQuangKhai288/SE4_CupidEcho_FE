e22c2e4445f3a6bd7fead72611041e573473b419
TranQuangKhai288 <21522185@gm.uit.edu.vn>
2025-05-27 22:11:52 +0700

    update context usesr

--- src/apis/UserAPI.ts
+++ src/apis/UserAPI.ts
@@ -48,10 +48,10 @@ export const getUserRecommends = async () => {
   return res;
 };
 
-export const updateUser = async(data: any)=>{
-  const res:Response = await baseURL.put(`/user/`,data)
+export const updateUser = async (data: any) => {
+  const res: any = await baseURL.put(`/user/`, data);
   return res;
-}
+};
 
 export const refreshToken = async (refreshToken: string) => {
   const res: Response = await baseURL.post(`/user/refresh-token`, {
@@ -60,12 +60,12 @@ export const refreshToken = async (refreshToken: string) => {
   return res;
 };
 
-export const getConditionByUserId = async(userId: string)=>{
-  const res:Response = await baseURL.get(`/user/condition/${userId}`);
+export const getConditionByUserId = async (userId: string) => {
+  const res: Response = await baseURL.get(`/user/condition/${userId}`);
   return res.data;
-}
+};
 
-export const updateConditionByUser = async(data: any)=>{
-  const res:Response = await baseURL.put(`/user/condition/`,data);
+export const updateConditionByUser = async (data: any) => {
+  const res: Response = await baseURL.put(`/user/condition/`, data);
   return res;
-}
+};
